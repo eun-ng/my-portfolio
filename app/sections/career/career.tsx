13bded0410cd0eebe7a55f0de4f1af44dbd90195
Eun-Ng <eunng.dev@gmail.com>
2025-07-27 21:09:59 +0900

    fix: career 내용 추가

--- app/sections/career/career.tsx
+++ app/sections/career/career.tsx
@@ -26,12 +26,42 @@ const CAREER_INFO: CareerInfo[] = [
           개발했습니다. 기획자, 디자이너, PM과 긴밀히 협업하며 레거시 프로젝트 개선부터 신규 서비스 개발까지 전 과정에
           참여하여 제품 고도화를 지속적으로 수행했습니다.
         </p>
-        <ul className="list-disc pl-5 mt-2 space-y-1">
-          <li>채용 관리 솔루션(ATS) MVP 신규 개발 및 디자인 시스템 개발</li>
-          <li>채용 솔루션 FAIRY 개발</li>
-          <li>채용 솔루션 RAVIS 개발</li>
-          <li>채용 솔루션 FAIRY 기업용 채용 홈페이지 마이그레이션</li>
-          <li>인성검사 설정 어드민 페이지 개발</li>
+        <ul className="list-none pl-5 mt-2 space-y-4">
+          <li>
+            채용 관리 솔루션(ATS) MVP 신규 개발
+            <ul className="list-disc pl-5 mt-1 space-y-1">
+              <li>디자인 시스템 설계 및 개발</li>
+              <li>채용 자동화 처리 칸반 보드 개발</li>
+              <li>공통 컴포넌트, 레이아웃 설계 및 개발</li>
+              <li>채용과정 자동화 처리 칸반 보드 개발</li>
+            </ul>
+          </li>
+          <li>
+            채용 솔루션 FAIRY 개발
+            <ul className="list-disc pl-5 mt-1 space-y-1">
+              <li>JavaScript {`->`} TypeScript로 마이그레이션</li>
+              <li>공통 컴포넌트, 레이아웃 설계 및 개발</li>
+              <li>레거시 코드 리팩토링 & 신규 기능 개발</li>
+            </ul>
+          </li>
+          <li>
+            채용 솔루션 RAVIS 개발
+            <ul className="list-disc pl-5 mt-1 space-y-1">
+              <li>반응형 디자인으로 전환</li>
+              <li>공통 컴포넌트, 레이아웃 설계 및 개발</li>
+              <li>레거시 코드 리팩토링 & 신규 기능 개발</li>
+            </ul>
+          </li>
+          <li>
+            채용 솔루션 FAIRY 기업용 채용 홈페이지 마이그레이션
+            <ul className="list-disc pl-5 mt-1 space-y-1">
+              <li>Next.js v13 pages router {`->`} v14 app router로 마이그레이션</li>
+              <li>Turbopack 사용으로 로컬 개발 환경 속도 개선</li>
+              <li>이미지, 폰트 최적화 문제 개선</li>
+              <li>프리 렌더링 & SEO 개선</li>
+              <li>K6 활용한 부하테스트 진행</li>
+            </ul>
+          </li>
         </ul>
       </>
     ),
